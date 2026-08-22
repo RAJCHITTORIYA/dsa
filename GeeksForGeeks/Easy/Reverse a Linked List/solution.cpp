9fd872a97494c3d1c4348cc85dc7d69a760ff6f4
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-08-22 15:57:51 +0530

    Solved: Reverse a Linked List [Easy]

--- GeeksForGeeks/Easy/Reverse a Linked List/solution.cpp
+++ GeeksForGeeks/Easy/Reverse a Linked List/solution.cpp
@@ -12,24 +12,39 @@ class Node {
 
 class Solution {
     Node reverseList(Node head) {
-        Node temp = head;
-        ArrayList <Node> arr = new ArrayList<>();
         
-        while(temp != null)
-        {
-            arr.add(temp);
-            temp = temp.next;
+        // Node temp = head;
+        // ArrayList <Node> arr = new ArrayList<>();
+        
+        // while(temp != null)
+        // {
+        //     arr.add(temp);
+        //     temp = temp.next;
             
-        }
-        int n = arr.size();
-        for(int i = n-1 ; i>=1 ; i--)
-        {
-            Node t1 = arr.get(i);
-            Node t2 = arr.get(i-1);
-            t1.next = t2;
+        // }
+        // int n = arr.size();
+        // for(int i = n-1 ; i>=1 ; i--)
+        // {
+        //     Node t1 = arr.get(i);
+        //     Node t2 = arr.get(i-1);
+        //     t1.next = t2;
             
+        // }
+        // arr.get(0).next = null;
+        // return arr.get(n-1);
+        
+        Node prev = null;
+        Node curr = head;
+        Node fwd = null;
+        while( curr!= null)
+        {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
         }
-        arr.get(0).next = null;
-        return arr.get(n-1);
+        return prev;
+        
     }
+    
 }
