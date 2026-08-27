e049c084a1cedd2a3f413f991faa89870d53b291
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-08-27 16:37:49 +0530

    Solved: Merge K Sorted Linked Lists [Medium]

---
+++ GeeksForGeeks/Medium/Merge K Sorted Linked Lists/solution.cpp
@@ -0,0 +1,72 @@
+/* Linked List Node Structure
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    Node mergeKLists(Node[] lists) {
+        
+        
+        
+        if(lists.length == 0) return null;
+
+            ArrayList <Node> arr = new ArrayList<>();
+
+            for   (Node n : lists)  arr.add(n);
+
+            while(arr.size() > 1)
+            {
+               Node a = arr.get(arr.size() - 1);
+                arr.remove(arr.size() -1 );
+
+               Node b = arr.get(arr.size() - 1);
+                arr.remove(arr.size() -1 );
+
+               Node c = merge(a,b);
+                arr.add(c);
+            } 
+                return arr.get(0);
+        }
+       Node merge(Node head1 , Node head2)
+        {
+           Node i = head1;
+           Node j = head2;
+
+            Node dummy = new Node(-1);
+            Node k = dummy;
+
+            while(i!= null && j!= null)
+            {
+                if(i!= null && j!=null)
+                {
+                    if(i.data <= j.data)
+                    {
+                        k.next = i;
+                        i = i.next;
+                    }else
+                    {
+                        k.next = j;
+                        j = j.next;
+                    }
+                    k = k.next;
+                }
+                if(i == null) k.next = j;
+                else
+                    k.next = i;
+            }
+             if (i != null) {
+            k.next = i;
+        } 
+        else {
+            k.next = j;
+        }
+            return dummy.next;
+            
+            
+    }
+}
