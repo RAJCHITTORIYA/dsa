8291d4110ddb856f3986e8ed6223085a52207239
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-01 22:26:09 +0530

    Solved: Reverse a sublist of a linked list [Hard]

--- GeeksForGeeks/Hard/Reverse a sublist of a linked list/solution.cpp
+++ GeeksForGeeks/Hard/Reverse a sublist of a linked list/solution.cpp
@@ -11,33 +11,78 @@ class Node {
 */
 
 class Solution {
-    Node reverseBetween(int a, int b, Node head) {
-        ArrayList<Node> arr = new ArrayList<>();
-        Node temp = head;
-        while(temp!= null)
+    Node reverseBetween(int l, int r, Node head) {
+        // ArrayList<Node> arr = new ArrayList<>();
+        // Node temp = head;
+        // while(temp!= null)
+        // {
+        //     arr.add(temp);
+        //     temp = temp.next;
+        // }
+        // int i = a-1 , j = b-1;
+        // while(i<j)
+        // {
+        //     Node t1 = arr.get(i);
+        //     Node t2 = arr.get(j);
+            
+        //     arr.set(i,t2);
+        //     arr.set(j,t1);
+        //     i++;
+        //     j--;
+        // }
+        
+        // for(i = 0 ; i<arr.size();i++)
+        // {
+        //     arr.get(i).next = (i== arr.size()-1) ? null : arr.get(i+1);
+            
+        // }
+        
+        // return arr.get(0);
+        
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        dummy.next = head;
+        
+        for(int i = 0 ; i< l-1 ; i++)
         {
-            arr.add(temp);
             temp = temp.next;
-        }
-        int i = a-1 , j = b-1;
-        while(i<j)
-        {
-            Node t1 = arr.get(i);
-            Node t2 = arr.get(j);
-            
-            arr.set(i,t2);
-            arr.set(j,t1);
-            i++;
-            j--;
-        }
-        
-        for(i = 0 ; i<arr.size();i++)
-        {
-            arr.get(i).next = (i== arr.size()-1) ? null : arr.get(i+1);
             
         }
+        Node tail1 = temp;
+        Node head2 = temp.next;
         
-        return arr.get(0);
+        
+        for(int i = 1 ; i<=r-l+1; i++)
+        {
+           temp = temp.next; 
+        }
+        Node tail2 = temp;
+        Node head3 = temp.next;
+        tail2.next = null;
+        tail1.next = null;
+        reverse(head2);
+        
+        tail1.next = tail2;
+        head2.next = head3;
+        return dummy.next;
+        
+    }
+    
+    public static Node reverse(Node head)
+    {
+        Node prev = null;
+        Node curr = head;
+        Node fwd = null;
+        
+        while(curr != null)
+        {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+    
+        return prev;
     }
 }
 
