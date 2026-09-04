9fe3bdb127487bc061251f77573d2527fddc7573
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-04 16:04:19 +0530

    Solved: Reverse a Doubly Linked List [Easy]

---
+++ GeeksForGeeks/Easy/Reverse a Doubly Linked List/solution.cpp
@@ -0,0 +1,32 @@
+/* Structure of Doubly Linked List Node
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public Node reverse(Node head) {
+        Node prev = null;
+        Node fwd = null;
+        Node  curr = head;
+        
+        while(curr!= null)
+        {
+            fwd = curr.next;
+            curr.next = prev;
+            curr.prev = fwd;
+            prev = curr;
+            curr = fwd;
+        }
+        
+        return prev  ;
+        
+    }
+}
