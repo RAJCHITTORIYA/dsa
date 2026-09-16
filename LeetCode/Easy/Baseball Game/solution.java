11f1811ec7154cdc3b51c04c8a35c4dcba25e8d7
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-16 10:42:09 +0530

    Solved: Baseball Game [Easy]

---
+++ LeetCode/Easy/Baseball Game/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int calPoints(String[] operations) {
+int[] stack = new int[operations.length];
+        int top = -1;
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+                top--;
+            }
+
+            else if (op.equals("D")) {
+                stack[++top] = 2 * stack[top - 1];
+            }
+
+            else if (op.equals("+")) {
+                stack[++top] = stack[top - 1] + stack[top - 2];
+            }
+
+            else {
+                stack[++top] = Integer.parseInt(op);
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i <= top; i++) {
+            sum += stack[i];
+        }
+
+        return sum;
+
+        
+    }
+}
