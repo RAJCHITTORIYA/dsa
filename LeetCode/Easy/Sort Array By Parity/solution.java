f9fb4f181410070808d735bbf2edb240e6738742
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-18 18:53:57 +0530

    Solved: Sort Array By Parity [Easy]

---
+++ LeetCode/Easy/Sort Array By Parity/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums;
+        int i = 0 , j = n-1;
+        while(i<j)
+        {
+            if(nums[i] %2 !=0)
+            {
+                if(nums[j]%2 == 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    j--;
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }   
+            }
+            else
+            {
+                i++;
+            }  
+        }
+      return nums;  
+    }
+}
