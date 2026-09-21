57ffb1eae1799a968893af6edeb208d9e697a807
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-21 12:24:54 +0530

    Solved: Matrix Diagonal Sum [Easy]

--- LeetCode/Easy/Matrix Diagonal Sum/solution.java
+++ LeetCode/Easy/Matrix Diagonal Sum/solution.java
@@ -4,16 +4,26 @@ class Solution {
         int m = mat[0].length;
         int sum = 0;
 
-        for(int i = 0 ; i<n ;i++)
+        for(int i = 0; i<n; i++)
         {
-            sum +=mat[i][i];
+            
+            for(int j = 0 ; j<m; j++)
+            {
+           
+                if(i == j )
+                {
+                    sum +=mat[i][j];
+                }
+                if(i+j == n-1)
+                {
+                    if(i==j)
+                    {
+                        continue;
+                    }
+                    sum+=mat[i][j];
+                }
 
-            sum+=mat[i][n-1-i];
-
-        }
-        if(n%2 == 1)
-        {
-            sum -= mat[n/2][n/2];
+            }
         }
         return sum;
         
