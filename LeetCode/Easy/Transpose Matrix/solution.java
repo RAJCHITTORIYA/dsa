3fa4fb9f0c3a93f5ab90a721806227123cdd8a76
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-21 13:14:17 +0530

    Solved: Transpose Matrix [Easy]

---
+++ LeetCode/Easy/Transpose Matrix/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] transpose(int[][] mat) {
+
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] arr = new int[m][n];
+
+        for(int i = 0; i<arr.length; i++)
+        {
+            for(int j = 0; j< arr[0].length; j++)
+            {
+                arr[i][j] = mat[j][i];
+                
+            }
+        }
+        return arr;
+        
+    }
+}
