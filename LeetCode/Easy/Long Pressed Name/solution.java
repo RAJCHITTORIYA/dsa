fb894aab7926e9c2a8f7f1e3d285977afcb0cb18
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2026-09-22 10:06:22 +0530

    Solved: Long Pressed Name [Easy]

--- LeetCode/Easy/Long Pressed Name/solution.java
+++ LeetCode/Easy/Long Pressed Name/solution.java
@@ -1,33 +1,38 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
 
-           int i = 0; // name pointer
-        int j = 0; // typed pointer
+        int n = name.length();
+        int m = typed.length();
 
-        while (j < typed.length()) {
+        int i = 0;
+        int j = 0;
 
-            // Normal character match
-            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+        while(i < n && j<m)
+        {
+            if(name.charAt(i) == typed.charAt(j)){
                 i++;
                 j++;
             }
-
-            // Long pressed character
-            else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
-                j++;
-            }
-
-            // Invalid character
-            else {
+            else if(j > 0 && typed.charAt(j) == typed.charAt(j - 1)){
+                j++;  
+            } 
+            else
+            {
                 return false;
-            }
+            }     
         }
+        
+        if(i == n) {
+    while(j < m) {
+        if(typed.charAt(j) != typed.charAt(j - 1)) {
+            return false;
+        }
+        j++;
+    }
+    return true;
+}
 
-        // Name ke saare characters consume hone chahiye
-        return i == name.length();
-
-    
-
+return false;
 
         
     }
